0201f6a05eff6904cdf55ba57af566681d54e4a4
PavaniChitturi04 <130127616+PavaniChitturi04@users.noreply.github.com>
2024-04-19 08:34:32 +0530

    Update AutokeyCipher.py

--- AutokeyCipher.py
+++ AutokeyCipher.py
@@ -1,45 +1,51 @@
-def autokey_encryption(msg, key):
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    length = len(msg)
+#Write a C/JAVA program to implement AutoKey Cipher
 
-    # Generating the keystream
-    new_key = key + msg
-    new_key = new_key[:len(new_key) - len(key)]
-    encrypted_msg = ""
+def generate_key(keyword, text):
+    keyword = keyword.upper()
+    keyword = list(keyword)
+    if len(keyword) == len(text):
+        return keyword
+    else:
+        for i in range(len(text) - len(keyword)):
+            keyword.append(text[i])
+    return ''.join(keyword)
 
-    # Applying encryption algorithm
-    for x in range(length):
-        first = alphabet.index(msg[x])
-        second = alphabet.index(new_key[x])
-        total = (first + second) % 26
-        encrypted_msg += alphabet[total]
+def autokey_cipher_encrypt(plaintext, keyword):
+    plaintext = plaintext.upper()
+    keyword = keyword.upper()
+    key = generate_key(keyword, plaintext)
+    ciphertext = ''
+    for i in range(len(plaintext)):
+        if plaintext[i].isalpha():
+            shift = (ord(plaintext[i]) + ord(key[i])) % 26
+            ciphertext += chr(shift + 65)
+        else:
+            ciphertext += plaintext[i]
+    return ciphertext
 
-    return encrypted_msg
+def autokey_cipher_decrypt(ciphertext, keyword):
+    ciphertext = ciphertext.upper()
+    keyword = keyword.upper()
+    key = generate_key(keyword, ciphertext)
+    plaintext = ''
+    for i in range(len(ciphertext)):
+        if ciphertext[i].isalpha():
+            shift = (ord(ciphertext[i]) - ord(key[i]) + 26) % 26
+            plaintext += chr(shift + 65)
+            key += chr(shift + 65)
+        else:
+            plaintext += ciphertext[i]
+            key += ciphertext[i]
+    return plaintext
 
+# Get user input for plaintext and keyword
+plaintext = input("Enter the plaintext: ")
+keyword = input("Enter the keyword: ")
 
-def autokey_decryption(msg, key):
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    current_key = key
-    decrypted_msg = ""
+# Encrypt the plaintext
+encrypted_text = autokey_cipher_encrypt(plaintext, keyword)
+print("Encrypted Text:", encrypted_text)
 
-    # Applying decryption algorithm
-    for x in range(len(msg)):
-        get1 = alphabet.index(msg[x])
-        get2 = alphabet.index(current_key[x])
-        total = (get1 - get2) % 26
-        total = (total + 26) if total < 0 else total
-        decrypted_msg += alphabet[total]
-        current_key += alphabet[total]
-
-    return decrypted_msg
-
-
-# Example usage:
-msg = input("Enter the plain text: ")
-key = input("Enter the key: ")
-
-encrypted_msg = autokey_encryption(msg, key)
-decrypted_msg = autokey_decryption(encrypted_msg, key)
-
-print(f"Encrypted: {encrypted_msg}")
-print(f"Decrypted: {decrypted_msg}")
+# Decrypt the ciphertext
+decrypted_text = autokey_cipher_decrypt(encrypted_text, keyword)
+print("Decrypted Text:", decrypted_text)
